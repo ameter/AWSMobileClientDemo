2a59ce44e0363b51783a817db0cdc5534522ba89
Chris Ameter <29186822+ameter@users.noreply.github.com>
2022-06-11 09:52:30 -0600

    Initial Commit

---
+++ AWSMobileClientDemo/AWSMobileClientDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSMobileClientDemoApp.swift
+//  AWSMobileClientDemo
+//
+//  Created by Ameter, Chris on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AWSMobileClientDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
